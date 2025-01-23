ce843df5e030f54bad769123b3584ec0fe145263
mahendran7498 <mahendranjaya7498@gmail.com>
2025-01-23 12:14:14 +0530

    Colleges added

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ class Config():
     # MYSQL_USER = 'root'                  # MySQL username
     # MYSQL_PASSWORD = 'db_pass'       # MySQL password (change it to your actual password) 147.79.68.252
     MYSQL_HOST = '147.79.68.252'       # MySQL server host
-    MYSQL_DATABASE = 'mailerjobs'       # Your MySQL database name
-    MYSQL_USER = 'root'         # MySQL username
+    MYSQL_DATABASE = 'new_mj_db'       # Your MySQL database name
+    MYSQL_USER = 'new_mj_user'         # MySQL username
     MYSQL_PASSWORD = 'Nextlearn@123'   # MySQL password (change it to your actual password)
     # SECRET_KEY = "d9a6d1f1f5dab18e3659868484ccc85a"
     # current_dir = os.getcwd()
@@ -52,9 +52,9 @@ os.makedirs(Config.COLLEGE_RESUME_FOLDER,exist_ok=True)
 class TestingConfig:
     TESTING = True
     MYSQL_HOST = "localhost"
-    MYSQL_DATABASE = 'mahi'
+    MYSQL_DATABASE = 'mailerjobs_test'
     MYSQL_USER = 'root'              # MySQL username
-    MYSQL_PASSWORD = 'root' 
+    MYSQL_PASSWORD = 'Chandra_121' 
 
     # C:\Users\visha\OneDrive\Documents\dumps\Dump20241211
 
