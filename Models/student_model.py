6890d3eb7c4b13373b1ae7ae5d8927f31c5cf6a3
omkarkale <omkarkalekerur2002@gmail.com>
2025-03-10 12:25:27 +0530

    some minor changes in backend

--- Models/student_model.py
+++ Models/student_model.py
@@ -54,7 +54,7 @@ def get_all_students_by_college(college_name,id=0):
     SELECT student_id, name, dob, gender, phone, email, institution, degree, graduation_year, reg_no, resume_name
     FROM students
     """
-    cursor.execute(query, (college_name,id))
+    cursor.execute(query)
     students = cursor.fetchall()
     conn.close()
 
